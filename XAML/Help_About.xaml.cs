e3b9b16cdcf2efc60b5d8c63585f2705bca39bef
Marcus Beaty <mbeaty@cardinalsolutions.com>
2018-11-08 11:03:59 -0500

    -Added Help_About files

---
+++ XAML/Help_About.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SimpleTaskTracker.XAML
+{
+    /// <summary>
+    /// Interaction logic for Help_About.xaml
+    /// </summary>
+    public partial class Help_About : UserControl
+    {
+        public Help_About()
+        {
+            InitializeComponent();
+        }
+
+        private void Donate_Btn_Click(object sender, RoutedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("http://www.paypal.me/MarcusBeaty");
+        }
+
+        private void Help_Btn_Click(object sender, RoutedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://drive.google.com/file/d/1x0L-HiZkMyExT4Y2T9BVTw7xTtv31Kq1/view?usp=sharing");
+        }
+    }
+}
