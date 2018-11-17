c4f53c2ac77aaa8c478b51ed7448b545f3c84fee
Marcus <marcusbeaty2187@yahoo.com>
2018-11-16 19:51:16 -0500

    .NET 4.7

--- Database/Data.Designer.cs
+++ Database/Data.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\mbeaty\Documents\Visual Studio 2015\Projects\SimpleTaskTracker\Database\Data.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\Marcu\source\repos\SimpleTaskTracker\Database\Data.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
